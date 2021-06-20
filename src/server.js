4123d92e34ecfeb01c922bcad46e1e16354d5a07
Adrian Braga <adrian.braga.ro@gmail.com>
2021-06-20 16:15:34 -0400

    Configura a view engine nunjucks

--- src/server.js
+++ src/server.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-console */
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const server = express();
 
 const routes = require('./routes');
 
 server.use(express.urlencoded({ extended: true }));
+server.use(express.static('./public'));
 server.use(routes);
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('./src/views', {
+  express: server,
+  autoescape: false,
+  noCache: false,
+  watch: true,
+});
 
 server.listen(5000, () => {
   console.log('Servidor rodando em https://localhost:5000');
