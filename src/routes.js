8b86f5279db7404dbb94f31005bf5bcf980691a4
Adrian Braga <adrian.braga.ro@gmail.com>
2021-07-09 11:43:11 -0400

    Extraindo dados do JSON

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const data = require('./data.json');
+
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-  return res.render('index');
+  return res.render('index', { donors: data.donors });
 });
 
 module.exports = routes;
