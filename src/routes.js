21ed9b719e0b1e81c4d3fe15fdb1d9077b89e453
Adrian Braga <adrian.braga.ro@gmail.com>
2021-07-10 13:11:24 -0400

    Cadastrando doadores

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,35 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
 const express = require('express');
+const fs = require('fs');
 
-const data = require('./data.json');
+const data = require('../data.json');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
   return res.render('index', { donors: data.donors });
+});
+
+routes.post('/', (req, res) => {
+  const { name, email, blood } = req.body;
+
+  const id = Number(data.donors.length + 1);
+
+  data.donors.push({
+    id,
+    name,
+    email,
+    blood,
+  });
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function (err) {
+    if (err) return res.send('Error! Falha na escrita dos dados!');
+  });
+
+  return res.redirect('/');
 });
 
 module.exports = routes;
